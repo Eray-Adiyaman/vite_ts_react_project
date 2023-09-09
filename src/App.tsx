6e5f1aeaf487110d84bf71b340bc81efa157a1a7
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-09-09 15:46:45 +0300

    several react hooks usages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,61 @@
-import { useState } from "react";
-import Counter from "./components/Counter";
-import Heading from "./components/Heading";
-import { Section } from "./components/Section";
-import List from "./components/List";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  MouseEvent,
+  KeyboardEvent,
+  useMemo,
+  useRef,
+} from "react";
+
+interface User {
+  id: number;
+  username: string;
+}
+
+type FibboFunc = (n: number) => number;
+
+const fib: FibboFunc = (n) => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const fibnum: number = 23;
 
 function App() {
   const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  console.log(inputRef?.current)
+  console.log(inputRef?.current?.value)
+
+  useEffect(() => {
+    console.log("mounting");
+    console.log("Users:", users);
+
+    return () => console.log("unmounting");
+  }, [users]);
+
+  const addTwo = useCallback(
+    (
+      e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+    ): void => {
+      e.preventDefault(),
+      setCount((prev) => prev + 2);
+    },
+    []
+  );
+
+  const fiboResult = useMemo(() => fib(fibnum), [fibnum]);
 
   return (
     <>
-      <Heading title={"Hey"} />
-      <Section title="another title">section element children</Section>
-      <Counter setCount={setCount}>Count is {count}</Counter>
-      <List
-        items={["coffee", "milk", "dust", "water"]}
-        render={(item: string) => <span className="gold">{item}</span>}
-      />
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>Add</button>
+      <button onClick={addTwo}>+2</button>
+      <h1>{fiboResult}</h1>
+      <input ref={inputRef} type="text" />
     </>
   );
 }
