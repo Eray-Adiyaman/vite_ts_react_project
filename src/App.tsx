57a443e69fdcb656d5d40ba62667555f4eca0ce3
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-09-09 15:03:33 +0300

    generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useState } from "react"
-import Counter from "./components/Counter"
-import Heading from "./components/Heading"
-import { Section } from "./components/Section"
-
+import { useState } from "react";
+import Counter from "./components/Counter";
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
+import List from "./components/List";
 
 function App() {
-   
-  const [count,setCount] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
 
   return (
     <>
-    <Heading title={"Hey"} />
-    <Section title="another title">section element children</Section>
-    <Counter setCount={setCount}  >Count is {count}</Counter>
+      <Heading title={"Hey"} />
+      <Section title="another title">section element children</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["coffee", "milk", "dust", "water"]}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
