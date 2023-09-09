b2422edebe11fb2b9658e4f00dac2b6718b6c44d
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-09-09 14:46:26 +0300

    basics_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Counter from "./components/Counter"
+import Heading from "./components/Heading"
+import { Section } from "./components/Section"
 
 
 function App() {
   
 
   return (
-   <h1>init</h1>
+    <>
+    <Heading title={"Hey"} />
+    <Section title="another title">section element children</Section>
+    <Counter></Counter>
+    </>
   )
 }
 
